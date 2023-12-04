8e391ffdbeda80d0f71e6b3350fde54995e0d44b
Victoria Le <95896163+levictoria0117@users.noreply.github.com>
2023-12-04 05:34:49 -0800

    Update main.py

--- TESTING/main.py
+++ TESTING/main.py
@@ -1,37 +1,19 @@
-import tkinter as ttk
 import customtkinter as ctk
+import webbrowser
 
-ctk.set_appearance_mode("Dark")  
-ctk.set_default_color_theme("blue")
+class Page(ctk.CTkFrame):
+    def __init__(self, *args, **kwargs):
+        ctk.CTkFrame.__init__(self, *args, **kwargs)
+    def show(self):
+        self.lift()
 
-class MainPage(ctk.CTk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("Title")
-        self.geometry(f"1100x580")
-
+class MainPage(Page):
+   def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
-
-        self.sidebar_frame = ctk.CTkFrame(self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        
-        self.title = ctk.CTkLabel(self.sidebar_frame, text="Movie Trends Analysis", font=ctk.CTkFont(size=16, weight="bold"))
-        self.title.grid(row=0, column=0, padx=20, pady=(20, 10))
-        
-        self.sidebar_button_1 = ctk.CTkButton(self.sidebar_frame, text="Home Page")
-        self.sidebar_button_1.grid(row=1, column=0, padx=10, pady=10)
-        self.sidebar_button_2 = ctk.CTkButton(self.sidebar_frame,text="Analysis Page")
-        self.sidebar_button_2.grid(row=2, column=0, padx=10, pady=10)
-        
-        self.theme_label = ctk.CTkLabel(self.sidebar_frame, text="Choose theme:", anchor="w")
-        self.theme_label.grid(row=5, column=0, padx=20, pady=(10, 0))
-        self.theme_optionemenu = ctk.CTkOptionMenu(self.sidebar_frame, values=["System","Light", "Dark"],
-                                                                      command=self.change_appearance_mode_event)
-        self.theme_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
         
         self.tabview = ctk.CTkTabview(self, width=250)
         self.tabview.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
@@ -49,8 +31,8 @@ class MainPage(ctk.CTk):
         self.label_rating = ctk.CTkLabel(self.tabview.tab("Rating"), text="Graph for rating")
         self.label_rating.grid(row=0, column=0, padx=20, pady=20)
 
-        self.scrollable_frame = ctk.CTkScrollableFrame(self, label_text="Movies")
-        self.scrollable_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.scrollable_frame = ctk.CTkScrollableFrame(self, width=250, label_text="Movies")
+        self.scrollable_frame.grid(row=1, column=1, padx=20, pady=20, sticky="nsew")
         self.scrollable_frame.grid_columnconfigure(0, weight=1)
         self.scrollable_frame_switches = []
         for i in range(50):
@@ -59,42 +41,65 @@ class MainPage(ctk.CTk):
             self.scrollable_frame_switches.append(label)
             
         self.sort = ctk.CTkFrame(self)
-        self.sort.grid(row=0, column=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.sort.grid(row=1, column=2, padx=10, pady=20, sticky="nsew")
         self.label_cb = ctk.CTkLabel(master=self.sort, text="Sort by:")
         self.label_cb.grid(row=0, column=0)
-        self.checkbox_1 = ctk.CTkCheckBox(master=self.sort, text="Option 1")
+        self.checkbox_1 = ctk.CTkCheckBox(master=self.sort, text="Title")
         self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
-        self.checkbox_2 = ctk.CTkCheckBox(master=self.sort, text="Option 2")
+        self.checkbox_2 = ctk.CTkCheckBox(master=self.sort, text="Rating")
         self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
         self.checkbox_3 = ctk.CTkCheckBox(master=self.sort, text="Option 3")
         self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
-    
-    def change_appearance_mode_event(self, new_appearance_mode: str):
-        ctk.set_appearance_mode(new_appearance_mode)
-        self.frames = {} 
+
+class HomePage(Page):
+   def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
         
-class HomePage(ctk.CTk):
-    def __init__(self):
-        super().__init__()
+        #Define a callback function
+        def callback(url):
+            webbrowser.open_new_tab(url)
+            
+        label = ctk.CTkLabel(self, text="Link to the Github repo.", cursor="hand2")
+        label.pack(side="top", fill="both", expand=True)
+        label.bind("<Button-1>", lambda e:
+            callback("https://github.com/NickN-24/CS122-Project"))
+       
+class EmptyPage(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = ctk.CTkLabel(self, text="empty page for extra things")
+       label.pack(side="top", fill="both", expand=True)
 
-        self.title("Title")
-        self.geometry(f"1100x580")
+class View(ctk.CTkFrame):
+    def __init__(self, *args, **kwargs):
+        ctk.CTkFrame.__init__(self, *args, **kwargs)
+        p1 = HomePage(self)
+        p2 = MainPage(self)
+        p3 = EmptyPage(self)
 
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
-        self.grid_rowconfigure((0, 1, 2), weight=1)
+        buttonframe = ctk.CTkFrame(self)
+        container = ctk.CTkFrame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
 
-        self.sidebar_frame = ctk.CTkFrame(self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        
-        self.title = ctk.CTkLabel(self.sidebar_frame, text="Movie Trends Analysis", font=ctk.CTkFont(size=16, weight="bold"))
-        self.title.grid(row=0, column=0, padx=20, pady=(20, 10))
-        
-        self.sidebar_button_1 = ctk.CTkButton(self.sidebar_frame, text="Home Page")
-        self.sidebar_button_1.grid(row=1, column=0, padx=10, pady=10)
-        self.sidebar_button_2 = ctk.CTkButton(self.sidebar_frame,text="Analysis Page")
-        self.sidebar_button_2.grid(row=2, column=0, padx=10, pady=10)
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
-app = HomePage()
-app.mainloop()
+        b1 = ctk.CTkButton(buttonframe, text="Home Page", command=p1.show)
+        b2 = ctk.CTkButton(buttonframe, text="Analysis Page", command=p2.show)
+        b3 = ctk.CTkButton(buttonframe, text="Empty Page", command=p3.show)
+
+        b1.pack(side="left")
+        b2.pack(side="left")
+        b3.pack(side="left")
+
+        p1.show()
+
+if __name__ == "__main__":
+    root = ctk.CTk()
+    main = View(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("1280x720")
+    root.title("Movie Trends Analyzer")
+    root.mainloop()
