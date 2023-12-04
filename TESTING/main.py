b31d756fe88a57b84ca1c2e5f9e81101a0f3cb86
Victoria Le <95896163+levictoria0117@users.noreply.github.com>
2023-12-04 13:35:29 -0800

    Update main.py

--- TESTING/main.py
+++ TESTING/main.py
@@ -3,31 +3,34 @@ from analysis_page import AnalysisPage
 from data_page import DataPage
 from home_page import HomePage
 
+ctk.set_appearance_mode("Light")
+ctk.set_default_color_theme("dark-blue")
+
 class View(ctk.CTkFrame):
     def __init__(self, *args, **kwargs):
         ctk.CTkFrame.__init__(self, *args, **kwargs)
-        p1 = HomePage(self)
-        p2 = AnalysisPage(self)
-        p3 = DataPage(self)
+        home = HomePage(self)
+        analysis = AnalysisPage(self)
+        data = DataPage(self)
 
         buttonframe = ctk.CTkFrame(self)
         container = ctk.CTkFrame(self)
         buttonframe.pack(side="top", fill="x", expand=False)
         container.pack(side="top", fill="both", expand=True)
 
-        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        home.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        analysis.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        data.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
-        b1 = ctk.CTkButton(buttonframe, text="Home Page", command=p1.show)
-        b2 = ctk.CTkButton(buttonframe, text="Analysis", command=p2.show)
-        b3 = ctk.CTkButton(buttonframe, text="Data Management", command=p3.show)
-
-        b1.pack(side="left")
-        b2.pack(side="left")
-        b3.pack(side="left")
-
-        p1.show()
+        home_button = ctk.CTkButton(buttonframe, text="Home Page", command=home.show)
+        analysis_button = ctk.CTkButton(buttonframe, text="Analysis", command=analysis.show)
+        data_button = ctk.CTkButton(buttonframe, text="Data Management", command=data.show)
+            
+        home_button.pack(side="left")
+        analysis_button.pack(side="left")
+        data_button.pack(side="left")
+        
+        home.show()
 
 if __name__ == "__main__":
     root = ctk.CTk()
