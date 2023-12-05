f6d7576aaf0526817ebe259f97fcdf851d512642
Nick Nguyen <nick.nguyen@sjsu.edu>
2023-12-05 02:46:20 -0800

    DataPage - Slightly modified the UI for later cleaning

--- TESTING/data_page.py
+++ TESTING/data_page.py
@@ -9,28 +9,31 @@ class DataPage(Page) :
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
 
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
         self.menu_frame = ctk.CTkFrame(self, corner_radius=0)
-        self.menu_frame.grid(row=0, column=0, padx=(0,10), sticky="nsew")
-        self.delete_data = ctk.CTkButton(self.menu_frame, text="Delete Data", command=self.delete_selected)
-        self.delete_data.grid(row=1, column=0, padx=10, pady=20, sticky="nse")
-        self.retrieve_data = ctk.CTkButton(self.menu_frame, text="Retrieve Data", command=lambda:self.fetch_data(self.retrieve_text.get("0.0","end-1c")))
-        self.retrieve_data.grid(row=2, column=0, rowspan=2, padx=10, pady=10)
+        self.menu_frame.grid(row=0, column=0, rowspan=4, padx=20, pady=20, sticky="nsew")
+        self.menu_frame.grid_columnconfigure(0, weight=1)
 
+        self.delete_data = ctk.CTkButton(self.menu_frame, text="Delete Data", command=self.delete_selected)
+        self.delete_data.grid(row=0, column=0, padx=20, pady=(20,10), sticky="nsew")
         self.delete_text = ctk.CTkLabel(master=self.menu_frame, text="Delete the selected years")
-        self.delete_text.grid(row=1, column=1, padx=10, sticky="ew")
+        self.delete_text.grid(row=1, column=0, padx=20, sticky="ew")
+
+        self.retrieve_data = ctk.CTkButton(self.menu_frame, text="Retrieve Data", command=lambda:self.fetch_data(self.retrieve_text.get("0.0","end-1c")))
+        self.retrieve_data.grid(row=2, column=0, padx=10, pady=10)
         self.data_text = ctk.CTkLabel(master=self.menu_frame, text="Input the years to grab.\nie. 1980-1984,1987,2014-2012,2022")
-        self.data_text.grid(row=2, column=1, padx=10, sticky="ew")
+        self.data_text.grid(row=3, column=0, padx=10, pady=5, sticky="ew")
         self.retrieve_text = ctk.CTkTextbox(self.menu_frame, height=80)
         self.retrieve_text.insert("0.0", "1980-1984,1987,2014-2012,2022")
-        self.retrieve_text.grid(row=3, column=1, columnspan=4, padx=10, sticky="ew")
+        self.retrieve_text.grid(row=4, column=0, padx=10, pady=5, sticky="ew")
 
-        self.current_text = ctk.CTkLabel(master=self.menu_frame, text="Input : ")
-        self.current_text.grid(row=1, column=2, padx=10, sticky="ew")
+        self.current_text = ctk.CTkLabel(master=self.menu_frame, text="Input : N/A")
+        self.current_text.grid(row=5, column=0, padx=10, pady=5, sticky="ew")
         self.progress_text = ctk.CTkLabel(master=self.menu_frame, text="Progress : 0/0")
-        self.progress_text.grid(row=3, column=2, padx=10, sticky="ew")
+        self.progress_text.grid(row=6, column=0, padx=10, pady=5, sticky="ew")
 
         self.data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),"data")
         self.scrollable_frame = None
@@ -39,7 +42,7 @@ class DataPage(Page) :
 
     def delete_selected(self):
         self.scrollable_frame = ctk.CTkScrollableFrame(self, label_text="Data Years")
-        self.scrollable_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.scrollable_frame.grid(row=0, column=1, padx=10, pady=10, width=180, sticky="nse")
         self.scrollable_frame.grid_columnconfigure(0, weight=1)
 
         for check, label in self.scrollable_frame_switches:
@@ -85,7 +88,7 @@ class DataPage(Page) :
         if self.scrollable_frame != None :
             self.scrollable_frame.destroy()
         self.scrollable_frame = ctk.CTkScrollableFrame(self, label_text="Data Years")
-        self.scrollable_frame.grid(row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.scrollable_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nse")
         self.scrollable_frame.grid_columnconfigure(0, weight=1)
         if self.scrollable_frame_switches != None :
             for check, label in self.scrollable_frame_switches :
@@ -95,7 +98,7 @@ class DataPage(Page) :
         count=0
         for f in os.listdir(self.data_dir):
             if (re.compile(r'^\d{4}_movies\.pickle$')).match(f) :
-                label = ctk.CTkLabel(master=self.scrollable_frame, text=f"Year {f}")
+                label = ctk.CTkLabel(master=self.scrollable_frame, text=f"{f}")
                 label.grid(row=count, column=1, padx=10, pady=(0, 20), sticky="ew")
                 select = ctk.CTkCheckBox(master=self.scrollable_frame, text="")
                 select.grid(row=count, column=0, padx=10, pady=(0, 20), sticky="w")
